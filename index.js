f8473ab58a64f2dd79c9df4a3963995a2cf793b0
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-02-13 06:18:47 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const PORT = 3000
+const apiRoute = require('./routes/api')
+
+const path = require('path')
+
+const app = express()
+
+app.use('/api', apiRoute)
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(PORT, () => {
+    console.log(`Server run on PORT ${PORT}`)
+})
